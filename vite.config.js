46dbc3b8ce83fdfcf5abd26a8c426d82f8040bed
TrinhNhuAnh269 <92515122+TrinhNhuAnh269@users.noreply.github.com>
2025-08-15 15:29:33 +0700

    udpate

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/trinhnhuanh269.github.io/',
+  base: 'https://trinhnhuanh269.github.io/ourculture/',
   plugins: [react()],
 });
